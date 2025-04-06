2409043ca8a04804cc7b8c4b7ba5ee70697728f6
eudollyn <ehenriquesilva021@gmail.com>
2025-04-06 13:28:18 -0300

    Movendo projeto para nova pasta com build corrigido

---
+++ app/src/main/java/app/vercel/lista_de_tarefas_git_main_erick_carmos_projects/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.lista_de_tarefas_git_main_erick_carmos_projects.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
